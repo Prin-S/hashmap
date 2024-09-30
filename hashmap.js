0b07c0be381d5a82a23aace7e00393c69019a16a
Prin-S <98200861+Prin-S@users.noreply.github.com>
2024-09-30 14:48:24 +0700

    Added functions and ability to set load factor.
    Created remove(), length(), clear(), keys(), values(), and entries() functions.
    Allows setting of the hashmap load factor.

--- hashmap.js
+++ hashmap.js
@@ -1,7 +1,8 @@
-function createHashMap() {
-    const arr = new Array(16);
-    const loadFactor = 0.75;
-    const capacity = arr.length;
+function createHashMap(loadFactor) {
+    let buckets = new Array(16);
+    const bucketsLoadFactor = loadFactor;
+    const capacity = buckets.length;
+    let numOfStoredKeys = 0;
 
     function hash(key) {
         let hashCode = 0;
@@ -17,25 +18,29 @@ function createHashMap() {
     function set(key, value) {
         const hashCode = hash(key);
 
-        if (!arr[hashCode]) { // If the bucket is empty,
-            arr[hashCode] = [{ key, value }]; // Insert key and value as an object inside an array.
-        } else if (arr[hashCode].find(element => element.key == key)) { // If key already exists in the bucket,
-            arr[hashCode].find(element => element.key == key).value = value; // Change the value of that key to entered value. (find() returns the first element with key == entered key.)
-            console.log(`An existing key has been updated. Content of bucket ${hashCode}:`);
-            return arr[hashCode];
-        } else {
-            arr[hashCode].push({ key, value }); // Push a new object into the array.
+        if (!buckets[hashCode]) { // If the bucket is empty,
+            buckets[hashCode] = [{ key, value }]; // Insert key and value as an object inside an array and assign it to the bucket.
+            numOfStoredKeys++;
+        } else if (buckets[hashCode].find(element => element.key == key)) { // If key already exists in the bucket,
+            buckets[hashCode].find(element => element.key == key).value = value; // Change the value of that key to entered value. (find() returns the first element with key == entered key.)
+            console.log(`An existing key has been updated in ${hashCode}. Content of bucket ${hashCode}:`);
+
+            return buckets[hashCode];
+        } else { // If the bucket is not empty and key does not already exist in the bucket,
+            buckets[hashCode].push({ key, value }); // Push a new object into the bucket.
+            numOfStoredKeys++;
         }
 
-        console.log(`A new key has been added. Content of bucket ${hashCode}:`);
-        return arr[hashCode];
+        console.log(`A new key has been added to bucket ${hashCode}. Content of bucket ${hashCode}:`);
+
+        return buckets[hashCode];
     }
 
     function get(key) {
         const hashCode = hash(key);
 
-        if (arr[hashCode] && arr[hashCode].find(element => element.key == key)) { // If arr[hashCode] is truthy and key exists in the bucket,
-            return `The value of ${key} is ${arr[hashCode].find(element => element.key == key).value}.`; // Return value in { key, value }.
+        if (buckets[hashCode] && buckets[hashCode].find(element => element.key == key)) { // If buckets[hashCode] is truthy and key exists in the bucket,
+            return `The value of ${key} is ${buckets[hashCode].find(element => element.key == key).value}.`; // Return value in { key, value }.
         } else {
             return null;
         }
@@ -44,22 +49,101 @@ function createHashMap() {
     function has(key) {
         const hashCode = hash(key);
 
-        if (arr[hashCode] && arr[hashCode].find(element => element.key == key)) { // If arr[hashCode] is truthy and key exists in the bucket,
+        if (buckets[hashCode] && buckets[hashCode].find(element => element.key == key)) { // If buckets[hashCode] is truthy and key exists in the bucket,
             return true;
         } else {
             return false;
         }
     }
 
-    return { set, get, has };
+    function remove(key) {
+        const hashCode = hash(key);
+
+        if (buckets[hashCode] && buckets[hashCode].find(element => element.key == key)) { // If buckets[hashCode] is truthy and key exists in the bucket,
+            const elementToRemoveIndex = buckets[hashCode].findIndex(element => element.key == key); // Find the index of the element containing key so that we can splice it from the bucket.
+            numOfStoredKeys--;
+
+            console.log('Removed the following key-value pair from the bucket:');
+            console.log(buckets[hashCode].splice(elementToRemoveIndex, 1)[0]); // Splicing returns an array of the deleted element, so [0] is used to show just the removed object.
+            console.log(`Content of bucket ${hashCode}:`);
+            console.log(buckets[hashCode]);
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function length() {
+        console.log('Number of stored keys in the hashmap:');
+
+        return numOfStoredKeys;
+    }
+
+    function clear() {
+        buckets = [];
+        buckets = new Array(16);
+
+        return 'All entries have been removed from the hashmap.';
+    }
+
+    function keys() {
+        const returnArray = [];
+        
+        buckets.forEach(element => { // element = each bucket in the hashmap
+            if (element) {
+                element.forEach(bucketElement => returnArray.push(bucketElement.key)); // bucketElement = each element in a bucket
+            }
+        });
+
+        console.log('Keys in the hashmap:');
+
+        return returnArray;
+    }
+
+    function values() {
+        const returnArray = [];
+        
+        buckets.forEach(element => { // element = each bucket in the hashmap
+            if (element) {
+                element.forEach(bucketElement => returnArray.push(bucketElement.value)); // bucketElement = each element in a bucket
+            }
+        });
+
+        console.log('Values in the hashmap:');
+
+        return returnArray;
+    }
+
+    function entries() {
+        const returnArray = [];
+        
+        buckets.forEach(element => { // element = each bucket in the hashmap
+            if (element) {
+                element.forEach(bucketElement => returnArray.push([bucketElement.key, bucketElement.value])); // bucketElement = each element in a bucket
+            }
+        });
+
+        console.log('Key-value pairs in the hashmap:');
+
+        return returnArray;
+    }
+
+    return { set, get, has, remove, length, clear, keys, values, entries };
 }
 
-const newHashMap = createHashMap();
+const newHashMap = createHashMap(0.75);
 
 console.log(newHashMap.set('Carlos', 'I am the old value.'));
 console.log(newHashMap.set('Carlos', 'I am the new value.'));
 console.log(newHashMap.set('Cart', 'test'));
 console.log(newHashMap.set('Card', 'test 2'));
 console.log(newHashMap.set('Cat', '123'));
-console.log(newHashMap.get('Cat'));
-console.log(newHashMap.has('Carla'));
+console.log(newHashMap.get('Dog'));
+console.log(newHashMap.has('Cat'));
+console.log(newHashMap.keys());
+console.log(newHashMap.values());
+console.log(newHashMap.entries());
+console.log(newHashMap.remove('Cart'));
+console.log(newHashMap.length());
+console.log(newHashMap.clear());
